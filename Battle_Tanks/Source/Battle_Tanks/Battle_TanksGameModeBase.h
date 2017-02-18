73b0e229e557cd2672ce654e54665650f6b27813
GisliBG <gisligudmundss@gmail.com>
2017-02-18 00:12:22 +0000

    moved git ignore to working directory

---
+++ Battle_Tanks/Source/Battle_Tanks/Battle_TanksGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "Battle_TanksGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLE_TANKS_API ABattle_TanksGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
